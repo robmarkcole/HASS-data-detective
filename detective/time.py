5ed54aeddefff97556255bcc9635231b625177d5
Bram Kragten <mail@bramkragten.nl>
2018-12-17 21:20:33 +0100

    Check if data is a string

--- detective/time.py
+++ detective/time.py
@@ -41,8 +41,10 @@ def time_category(dtObj):
         return 'night'
 
 
-def sqlalch_datetime(dt_str):
+def sqlalch_datetime(dt):
     """Convert a SQLAlchemy datetime string to a datetime object."""
-    return datetime.strptime(
-        dt_str, '%Y-%m-%d %H:%M:%S.%f'
-    ).replace(tzinfo=UTC)
+    if isinstance(dt, str):
+        return datetime.strptime(
+            dt, '%Y-%m-%d %H:%M:%S.%f'
+        ).replace(tzinfo=UTC)
+    return dt
