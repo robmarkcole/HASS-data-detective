883ed9cd2a344e1bf3765ebc43eb8be70dcc6b00
robmarkcole <robmarkcole@gmail.com>
2018-12-21 07:09:41 +0000

    Release 0.9

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ REQUIRED = [
 
 setup(
     name='HASS-data-detective',
-    version='0.8',
+    version='0.9',
     packages=find_packages(exclude=('tests',)),
     url='https://github.com/robmarkcole/HASS-data-detective',
     keywords=['home', 'automation'],
