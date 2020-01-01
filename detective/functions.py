7f6b3b134475133c5ae262c83323351a3ae909b0
Robin Cole <robmarkcole@gmail.com>
2020-01-01 11:21:05 +0000

    Add more functions

--- detective/functions.py
+++ detective/functions.py
@@ -1,6 +1,9 @@
 """
 Helper functions.
 """
+import json
+import pandas as pd
+
 UNKNOWN = "unknown"
 
 
@@ -19,3 +22,39 @@ def get_device_class(attributes: dict):
     if device_class:
         return device_class
     return UNKNOWN
+
+
+def get_unit_of_measurement(attributes: dict):
+    """Return the unit_of_measurement."""
+    unit_of_measurement = attributes.get("unit_of_measurement")
+    if unit_of_measurement:
+        return unit_of_measurement
+    return UNKNOWN
+
+
+def get_friendly_name(attributes: dict):
+    """Return the friendly_name."""
+    friendly_name = attributes.get("friendly_name")
+    if friendly_name:
+        return friendly_name
+    return UNKNOWN
+
+
+def generate_features(df: pd.DataFrame) -> pd.DataFrame:
+    df["attributes"] = df["attributes"].apply(json.loads)
+    df["device_class"] = df["attributes"].apply(get_device_class)
+    df["unit_of_measurement"] = df["attributes"].apply(get_unit_of_measurement)
+    df["friendly_name"] = df["attributes"].apply(get_friendly_name)
+    return df
+
+
+def format_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    df["state"] = df["state"].apply(format_binary_state)
+    df["state"] = pd.to_numeric(
+        df["state"], errors="coerce"
+    )  # coerce will return NaN if unable to convert
+    df["last_changed"] = pd.to_datetime(
+        df["last_changed"].values, errors="ignore", utc=True
+    ).tz_localize(None)
+    df = df.dropna()
+    return df
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,7 +1,12 @@
 from detective import functions
 import math
 
-MOCK_ATTRIBUTE = {"friendly_name": "Office sensor motion", "device_class": "motion"}
+MOCK_ATTRIBUTE = {
+    "battery_level": 61,
+    "unit_of_measurement": "°C",
+    "friendly_name": "Living room sensor temperature",
+    "device_class": "temperature",
+}
 
 
 def test_format_binary_state():
@@ -12,6 +17,23 @@ def test_format_binary_state():
 
 
 def test_device_class():
-    """Test device_class"""
-    assert functions.get_device_class(MOCK_ATTRIBUTE) == "motion"
+    """Test get_device_class"""
+    assert functions.get_device_class(MOCK_ATTRIBUTE) == MOCK_ATTRIBUTE["device_class"]
     assert functions.get_device_class({}) == functions.UNKNOWN
+
+
+def test_get_unit_of_measurement():
+    """Test get_unit_of_measurement"""
+    assert (
+        functions.get_unit_of_measurement(MOCK_ATTRIBUTE)
+        == MOCK_ATTRIBUTE["unit_of_measurement"]
+    )
+    assert functions.get_unit_of_measurement({}) == functions.UNKNOWN
+
+
+def test_get_friendly_name():
+    """Test get_friendly_name"""
+    assert (
+        functions.get_friendly_name(MOCK_ATTRIBUTE) == MOCK_ATTRIBUTE["friendly_name"]
+    )
+    assert functions.get_friendly_name({}) == functions.UNKNOWN
